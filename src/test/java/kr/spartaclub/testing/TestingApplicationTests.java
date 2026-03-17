7734079d37365b73c6ad7df30559e662ebf0c85b
hoon3948 <robot3949@gmail.com>
2026-03-17 14:16:55 +0900

    dd

---
+++ src/test/java/kr/spartaclub/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
